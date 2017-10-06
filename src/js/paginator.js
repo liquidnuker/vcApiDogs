6dfab062b80268243a9abf029a4b2085d6dff169
administrator <liquidnuker@users.noreply.github.com>
2017-10-06 09:01:45 -0700

    added getter for masterBreedImages

---
+++ src/js/paginator.js
@@ -0,0 +1,29 @@
+import "./vendor/jPages.min.js";
+
+const pager = {
+  start: function (perPage) {
+    $("div.gallery-listing-paginator").jPages({
+      containerID: "itemContainer",
+      first: "first",
+      previous: "previous",
+      next: "next",
+      last: "last",
+      links: "numeric", // blank || title
+      delay: 0, // to remove fade
+      fallback: 0, // to remove fade
+      startPage: 1,
+      perPage: perPage,
+      midRange: 5,
+    });
+  },
+  activate: function (timeout, perPage) {
+    setTimeout(function () {
+      pager.start(perPage);
+    }, timeout);
+  },
+  destroy: function () {
+    $("div.gallery-listing-paginator").jPages("destroy");
+  }
+};
+
+export {pager};
--- src/vue-components/Home.vue
+++ src/vue-components/Home.vue
@@ -1,16 +1,58 @@
 <template>
   <div>
-    Home.vue
+    
   </div>
 </template>
 <script>
-  export default {
-    data () {
-      return {    
-      }
+import {axios_get} from "../js/axios_get.js";
+export default {
+  data() {
+    return {
+      allBreeds: [],
+      breedImages: [],
+      
+      // api urls
+      // List all breed names including sub breeds.
+      allBreedNames: "https://dog.ceo/api/breeds/list/all",
+      // Returns an array of all the dog images from all the master breeds
+      masterBreedImages: "https://dog.ceo/api/breeds/list",
+
+    }
+  },
+  mounted: function () {
+    // this.listAllBreedNames();
+    this.listMasterBreedImages();
+  },
+  methods: {
+    listAllBreedNames: function () {
+      let self = this;
+      axios_get(self.allBreedNames)
+        .then(function (response) {
+          let arr = Object.values(response);
+          let arr2 = arr[0].message;
+          
+          for (let i in arr2) {
+            self.allBreeds.push(i);
+          }
+        })
+        .then(function () {
+        // self.pagerActive = true;
+        // pager.activate(4, self.perPage); // timeOut, perPage
+      });
     },
-    mounted: function () {
-      console.log("Home.vue mounted");
+    listMasterBreedImages: function () {
+      // todo: fetch .jpg for this
+      let self = this;
+      axios_get(self.masterBreedImages)
+        .then(function (response) {
+          let arr = Object.values(response);
+          let arr2 = arr[0].message;
+        })
+        .then(function () {
+        // self.pagerActive = true;
+        // pager.activate(4, self.perPage); // timeOut, perPage
+      });
     }
   }
+}
 </script>
