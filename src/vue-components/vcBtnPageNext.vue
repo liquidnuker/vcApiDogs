7da5608e56463a1a9045d0de3ffd1853a789ae6f
administrator <liquidnuker@users.noreply.github.com>
2017-12-06 10:42:22 -0800

    changed base button styles

--- src/vue-components/vcBtnPageNext.vue
+++ src/vue-components/vcBtnPageNext.vue
@@ -1,11 +1,7 @@
 <template>
-  <div class="btn_pager">
-  <button class="btn btn_pager_previous" tabindex="0"
-  @click="nextPage()">
-  <span>Next</span>
-  <svg xmlns="http://www.w3.org/2000/svg"><path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"/></svg>
-</svg>
-</button>
+  <div>
+  <button class="btn btn1-01" tabindex="0"
+  @click="nextPage()">Next</button>
 </div>
 </template>
 <script>
--- src/vue-components/vcBtnPagePrev.vue
+++ src/vue-components/vcBtnPagePrev.vue
@@ -1,11 +1,7 @@
 <template>
-<div class="btn_pager">
-  <button class="btn btn_pager_previous" tabindex="0"
-  @click="prevPage()">
-  <svg xmlns="http://www.w3.org/2000/svg"><path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"/>
-</svg>
-<span>Previous</span>
-</button>
+<div>
+  <button class="btn btn1-01" tabindex="0"
+  @click="prevPage()">Previous</button>
 </div>
 </template>
 <script>
