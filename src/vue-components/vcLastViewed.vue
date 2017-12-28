196e78edd6e8813b4613e43bf276c991e1b23a53
administrator <liquidnuker@users.noreply.github.com>
2017-12-28 09:58:44 -0800

    modified leftside h2 markup

--- src/vue-components/vcLastViewed.vue
+++ src/vue-components/vcLastViewed.vue
@@ -2,7 +2,7 @@
 <div>
   <!--begin navSide103-->
   <nav class="navSide103" role="navigation">
-    <h3 class="navSide103_heading">Recently Viewed</h3>
+    <h2 class="navSide103_heading">Recently Viewed</h2>
     <ul>
       <li v-for="i in lastViewed">
         <div class="navSide103_img-holder">
--- src/vue-components/vcRandomDog.vue
+++ src/vue-components/vcRandomDog.vue
@@ -1,14 +1,14 @@
 <template>
 <div>
-  {{ prStatus }}
+  <h2 class="itemBox102_heading1">Random Dog</h2>
   <div class="itemBox102">
-    <h3 class="itemBox102_heading1">Random Dog</h3>
+    
     <div class="itemBox102_img-holder">
       <img :src="prRandomImage[0]" :alt="'random' + ' ' + prRandomBreed[0] + ' ' + 'image'" 
       :title="'random' + ' ' + prRandomBreed[0] + ' ' + 'image'">
       <!-- {{ prRandomImage[0] }} -->
     </div>
-    <h4 class="itemBox102_heading2">Name</h4>
+    <h3 class="itemBox102_heading2">Name</h3>
     <p>Breed: {{ prRandomBreed[0] }}</p>
   </div>
 </div>
