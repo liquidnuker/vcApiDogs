9865058e97137fc6ba8cb321d4b8a03ec0e968bc
administrator <liquidnuker@users.noreply.github.com>
2017-12-29 10:53:07 -0800

    modified img-holder img size on vcRandomDog

--- src/vue-components/vcRandomDog.vue
+++ src/vue-components/vcRandomDog.vue
@@ -5,8 +5,8 @@
     
     <div class="itemBox102_img-holder">
       <a :href="prRandomImage[0]">
-        <img :src="prRandomImage[0]" :alt="'random' + ' ' + prRandomBreed[0] + ' ' + 'image'" 
-      :title="'random' + ' ' + prRandomBreed[0] + ' ' + 'image'">
+        <img :src="prRandomImage[0]" :alt="'random' + ' ' + prRandomBreed[0] + ' ' + 'image'"
+        :title="'random' + ' ' + prRandomBreed[0] + ' ' + 'image'">
       </a>
       <!-- {{ prRandomImage[0] }} -->
     </div>
