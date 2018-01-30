e411b244751c76732e839a7e111beb259ba91104
administrator <liquidnuker@users.noreply.github.com>
2018-01-30 11:00:36 -0800

    modified/linted vcBreedDirectory.vue

--- src/vue-components/vcBreedDirectory.vue
+++ src/vue-components/vcBreedDirectory.vue
@@ -15,65 +15,55 @@
 </template>
 <script>
 export default {
-  data () {
-    return {   
-      breedDirItems: "",
-      breedDirChars: "",
-      breedDirList: []
-    }
-  },
-  watch: {
-  // : function () {
-  // }
-  },
-  props: [
-    "prBreedDirItems"
-  ],
-  components: {
-  },
-  mounted: function () {
-    this.breedDirItems = this.prBreedDirItems;
-    
-    this.breedDirChars = this.createDirLetters('a', 'z')
-    // console.log(this.breedDirChars);
-
-    this.extractBreeds(this.breedDirChars, this.breedDirItems);
-  },
-  methods: {   
-    createDirLetters: function (charA, charZ) {
-      let a = [];
-      let i = charA.charCodeAt(0);
-      let j = charZ.charCodeAt(0);
-      for (; i <= j; i++) {
-        a.push(String.fromCharCode(i));
-      }
-      return a;
+  data() {
+      return {
+        breedDirItems: "",
+        breedDirChars: "",
+        breedDirList: []
+      };
     },
-    extractBreeds: function(chars, arr) {
-      // extract breeds from dir letters
-      chars.map((i, index, array) => {
-        let z = this.extractLetters(arr, i);
-        
-        this.breedDirList.push({
-          name: z,
-          startsWith: i,
-          length: z.length
+    watch: {
+      // : function () {
+      // }
+    },
+    props: [
+      "prBreedDirItems"
+    ],
+    components: {},
+    mounted: function () {
+      this.breedDirItems = this.prBreedDirItems;
+      this.breedDirChars = this.createDirChars("a", "z");
+      this.extractBreeds(this.breedDirChars, this.breedDirItems, this.breedDirList);
+    },
+    methods: {
+      createDirChars: function (charA, charZ) {
+        let a = [];
+        let i = charA.charCodeAt(0);
+        let j = charZ.charCodeAt(0);
+        for (; i <= j; i++) {
+          a.push(String.fromCharCode(i));
+        }
+        return a;
+      },
+      extractBreeds: function (chars, items, dirList) {
+        // extract breeds from dir chars
+        chars.map((i) => {
+          let z = this.extractChars(items, i);
+
+          dirList.push({
+            name: z,
+            startsWith: i,
+            length: z.length
+          });
         });
-      });
-
-      // console.log(this.breedDirList[2].name); ok
-      // console.log(this.breedDirList.length); ok
-
-      console.dir(this.breedDirList);
-    },
-    extractLetters: function(arr, char) {
-      // returns array
-      let extracted = arr.filter(function (country) {
-        return country[0].toLowerCase() === char;
-      });
-
-      return extracted;
+      },
+      extractChars: function (arr, char) {
+        // returns array of items that starts with char
+        let extracted = arr.filter(function (el) {
+          return el[0].toLowerCase() === char;
+        });
+        return extracted;
+      }
     }
-  }
-}
+};
 </script>
