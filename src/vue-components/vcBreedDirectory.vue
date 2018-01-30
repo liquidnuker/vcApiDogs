ff4a3a3d73a031d0d224b00b737318c7fdbacb60
administrator <liquidnuker@users.noreply.github.com>
2018-01-30 10:02:54 -0800

    added temp breedDirList

--- src/vue-components/vcBreedDirectory.vue
+++ src/vue-components/vcBreedDirectory.vue
@@ -1,13 +1,25 @@
 <template>
   <div>
     breed directory
+
+    <div v-for="i in breedDirList">
+    {{ i.startsWith }}
+      <div v-for="z in i.name">
+        {{ z }} 
+      </div>
+      <br>
+        -------------------
+      <br>
+    </div>
   </div>
 </template>
 <script>
 export default {
   data () {
     return {   
-      c: ""
+      breedDirItems: "",
+      breedDirChars: "",
+      breedDirList: []
     }
   },
   watch: {
@@ -21,9 +33,47 @@ export default {
   },
   mounted: function () {
     this.breedDirItems = this.prBreedDirItems;
-    console.log(this.breedDirItems.length);
+    
+    this.breedDirChars = this.createDirLetters('a', 'z')
+    // console.log(this.breedDirChars);
+
+    this.extractBreeds(this.breedDirChars, this.breedDirItems);
   },
-  methods: {        
+  methods: {   
+    createDirLetters: function (charA, charZ) {
+      let a = [];
+      let i = charA.charCodeAt(0);
+      let j = charZ.charCodeAt(0);
+      for (; i <= j; i++) {
+        a.push(String.fromCharCode(i));
+      }
+      return a;
+    },
+    extractBreeds: function(chars, arr) {
+      // extract breeds from dir letters
+      chars.map((i, index, array) => {
+        let z = this.extractLetters(arr, i);
+        
+        this.breedDirList.push({
+          name: z,
+          startsWith: i,
+          length: z.length
+        });
+      });
+
+      // console.log(this.breedDirList[2].name); ok
+      // console.log(this.breedDirList.length); ok
+
+      console.dir(this.breedDirList);
+    },
+    extractLetters: function(arr, char) {
+      // returns array
+      let extracted = arr.filter(function (country) {
+        return country[0].toLowerCase() === char;
+      });
+
+      return extracted;
+    }
   }
 }
 </script>
