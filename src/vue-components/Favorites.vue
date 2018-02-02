0649dc00f208213cc08afceec50b33b575331376
administrator <liquidnuker@users.noreply.github.com>
2018-02-01 19:39:30 -0800

    changed layout of favorites list

--- src/vue-components/Favorites.vue
+++ src/vue-components/Favorites.vue
@@ -13,12 +13,12 @@
       <!-- leftside -->
       <!-- breed selector -->
       <div>
-<select class="breed-selector">
-  <option value="">Choose Breed...</option>
-  <option v-for="i in options"
-  @click="switchBreed(i)" :value="i">{{ i }}</option>
-</select>
-</div>
+        <select class="breed-selector">
+          <option value="">Choose Breed...</option>
+          <option v-for="i in options"
+          @click="switchBreed(i)" :value="i">{{ i }}</option>
+        </select>
+      </div>
       <!-- /breed selector -->
       <!-- randog -->
       <div>
@@ -55,9 +55,9 @@
             Prev</button>
             page
             <select @change="showPage($event.target.value)" class="pg_totalpages" v-model="currentPage">
-                <option v-for="i in totalPages" :value="i"
-                @click="showPage(i)">{{ i }}</option>
-              </select>
+              <option v-for="i in totalPages" :value="i"
+              @click="showPage(i)">{{ i }}</option>
+            </select>
             of {{ totalPages }}
             <button class="btn btn1-01 btn_next" tabindex="0"
             @click="nextPage()">Next
@@ -67,10 +67,10 @@
             </button>
           </span>
           <span class="favorites_filter">
-          <button class="btn btn1-01"
-          @click="showAll()">Show all breeds</button>
-          <!-- breed filter -->
-          <select class="breed-selector">
+            <button class="btn btn1-01"
+            @click="showAll()">Show all breeds</button>
+            <!-- breed filter -->
+            <select class="breed-selector">
               <option value="">Filter Breed...</option>
               <option v-for="i in favoriteCategories"
               @click="filter(i)" :value="i">{{ i }}</option>
@@ -80,18 +80,21 @@
         </div>
         <!-- /page controls -->
         <!-- favorites display -->
-        <div>
-          <ul >
+        <div class="favorites_list">
+          <ul>
             <li v-for="(i, index) in currentFavorites">
-              {{ i.breed }} {{ i.name }}
-              
-              <button class="btn btn1-01 btn_remove"
-              @click="removeItem(i.name)">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zm2.46-7.12l1.41-1.41L12 12.59l2.12-2.12 1.41 1.41L13.41 14l2.12 2.12-1.41 1.41L12 15.41l-2.12 2.12-1.41-1.41L10.59 14l-2.13-2.12zM15.5 4l-1-1h-5l-1 1H5v2h14V4z"/>
-              </svg>
-              </button>
-              <div v-show = "!i.edit">
+              <div class="favorites_list_info">
+                <p>{{ i.name }}</p>
+                <br>
+                <p>Breed: {{ i.breed }}</p>
+                <button class="btn btn1-01 btn_remove"
+                @click="removeItem(i.name)">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                  <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zm2.46-7.12l1.41-1.41L12 12.59l2.12-2.12 1.41 1.41L13.41 14l2.12 2.12-1.41 1.41L12 15.41l-2.12 2.12-1.41-1.41L10.59 14l-2.13-2.12zM15.5 4l-1-1h-5l-1 1H5v2h14V4z"/>
+                </svg>
+                </button>
+              </div>
+              <div class="favorites_notes" v-show = "!i.edit">
                 <label>{{ i.notes }}</label>
                 <button class="btn btn1-01 btn_edit"
                 v-show = "i.edit == false" @click="edit(i.name)">
@@ -100,9 +103,17 @@
                 </svg>
                 </button>
               </div>
-              <!-- v-on:blur= "i.edit = false" -->
-              <input v-show="i.edit" v-model="i.notes" @keyup.enter = "update(i.notes, i.name)">
-              <button class="btn btn1-01" v-show="i.edit" @click="cancelEdit(i.name)">cancel</button>
+              <div class="favorites_edit" v-show="i.edit">
+                <!-- v-on:blur= "i.edit = false" -->
+                <textarea class="col-sm-12" v-show="i.edit" v-model="i.notes" @keyup.enter = "update(i.notes, i.name)"></textarea>
+                <br>
+                <button class="btn btn1-01" v-show="i.edit" @click="update(i.notes, i.name)">
+                save
+                </button>
+                <button class="btn btn1-01" v-show="i.edit" @click="cancelEdit(i.name)">
+                cancel
+                </button>
+              </div>
             </li>
           </ul>
         </div>
@@ -140,154 +151,156 @@ const vcHeader = () => import ('./vcHeader.vue');
 const vcRandomDog = () => import ('./vcRandomDog.vue');
 const vcLastViewed = () => import ('./vcLastViewed.vue');
 export default {
-  data () {
-    return {   
-      filteredFavorites: "",
-      currentFavorites: "", // displayed items
+  data() {
+      return {
+        filteredFavorites: "",
+        currentFavorites: "", // displayed items
 
-      options: allbreeds.sort(),
-      favoriteCategories: "",
+        options: allbreeds.sort(),
+        favoriteCategories: "",
 
-      // paginator 
-      pager: null,
-      currentPage: "",
-      totalPages: "",
-      pagerButtons: true,
+        // paginator 
+        pager: null,
+        currentPage: "",
+        totalPages: "",
+        pagerButtons: true,
 
-      filterItem: null,
+        filterItem: null,
 
-      indexToEdit: null,
-      editNoteCache: null,
+        indexToEdit: null,
+        editNoteCache: null,
 
-      // random dog
-      randomDogBreed: "",
-      randomDogImage: "",
+        // random dog
+        randomDogBreed: "",
+        randomDogImage: "",
 
-      // status
-      status: {
-        randomDog: ""  
-      },
-      }
-  },
-  watch: {
-  // : function () {
-  // }
-  },
-  components: {
-    vcHeader: vcHeader,
-    vcRandomDog: vcRandomDog,
-    vcLastViewed: vcLastViewed,
-  },
-  mounted: function () {
-    this.filteredFavorites = store.favorites;
-    this.activatePager();
-
-    // for favorites dropdown
-    this.setFavoriteCategories(store.favorites);
-
-    this.showRandomDogImage();
-    this.status.randomDog = "loading random dog...";
-  },
-  methods: {
-    activatePager: function() {
-      this.pager = null;
-      this.pager = new Paginate(this.filteredFavorites); 
-      this.currentFavorites = this.pager.page(0);
-      this.currentPage = this.pager.currentPage; 
-      this.totalPages = this.pager.totalPages;
-      this.pagerButtons = true;
+        // status
+        status: {
+          randomDog: ""
+        },
+      };
     },
-    setFavoriteCategories: function(arr) {
-      this.favoriteCategories = [];
-      let categoryTemp = arr_extractUnique(arr, "breed");
-      
-      for (let i in categoryTemp) {
-        this.favoriteCategories.push(categoryTemp[i]);
-      }
-      categoryTemp = null;
+    watch: {
+      // : function () {
+      // }
     },
-    showPage: function(num) {
-      this.currentFavorites = this.pager.page(num);
+    components: {
+      vcHeader: vcHeader,
+      vcRandomDog: vcRandomDog,
+      vcLastViewed: vcLastViewed,
     },
-    nextPage: function() {
-      if (!this.pager.hasNext()) {
-        this.currentFavorites = this.pager.page(0);
-      } else {
-        this.currentFavorites = this.pager.next();
-      }
-      this.currentPage = this.pager.currentPage;
-    },
-    prevPage: function() {
-      if (this.pager.currentPage === 1) {
-        this.currentFavorites = this.pager.page(this.pager.totalPages);
-      } else {
-        this.currentFavorites = this.pager.prev();
-      }
-      this.currentPage = this.pager.currentPage;
-    },
-    edit: function(name) {
-      let index = nameExists(name, store.favorites);
-      this.indexToEdit = index;
-      this.editNoteCache = store.favorites[this.indexToEdit].notes; 
-      store.favorites[this.indexToEdit].edit = true;
-    },
-    cancelEdit: function(name) {
-      let index = nameExists(name, store.favorites);
-      store.favorites[index].edit = false;
-      store.favorites[index].notes = this.editNoteCache;
-      this.indexToEdit = null;
-    },
-    update: function(newNote, name) {
-      console.log("update");
-      let itemIndex = nameExists(name, store.favorites);
-      store.favorites[itemIndex].notes = newNote;
-      store.favorites[itemIndex].edit = false;
-    },
-    removeItem: function(name) {
-      let itemIndex = nameExists(name, store.favorites);
-      store.favorites.splice(itemIndex, 1);
+    mounted: function () {
       this.filteredFavorites = store.favorites;
-      
-      // stay on filtered view
-      if (this.filterItem) {
-        this.filter(this.filterItem);
-      } else {
-        this.showAll();
-      }
+      this.activatePager();
 
-      // update dropdown
+      // for favorites dropdown
       this.setFavoriteCategories(store.favorites);
-    },
-    filter: function(breed) {
-      this.filterItem = breed;
-      let filteredBreed = arr_filter(store.favorites, "breed", breed);
 
-      this.filteredFavorites = filteredBreed;
-      this.activatePager();
+      this.showRandomDogImage();
+      this.status.randomDog = "loading random dog...";
     },
-    showAll: function() {
-      this.filterItem = null;
-      this.filteredFavorites = store.favorites;
-      this.activatePager();
-    },
-    showRandomDogImage: function () {
-      this.randomDogBreed = shuffle(allbreeds);
-      // Returns an array of all the images from the breed
-      let url = "https://dog.ceo/api/breed/" + this.randomDogBreed[0] + "/images";
-      let self = this;
-      axios_get(url)
-        .then(function (response) {
-          let arr = Object.values(response);
-          self.status.randomDog = "";
-          self.randomDogImage = shuffle(arr[0].message);
-        })
-        .then(function () {
-        
-      });      
-    },
-    switchBreed: function(breed) {
-      router.push({path: "/gallery/" + breed});
-    }       
-  }
-}
+    methods: {
+      activatePager: function () {
+        this.pager = null;
+        this.pager = new Paginate(this.filteredFavorites);
+        this.currentFavorites = this.pager.page(0);
+        this.currentPage = this.pager.currentPage;
+        this.totalPages = this.pager.totalPages;
+        this.pagerButtons = true;
+      },
+      setFavoriteCategories: function (arr) {
+        this.favoriteCategories = [];
+        let categoryTemp = arr_extractUnique(arr, "breed");
+
+        for (let i in categoryTemp) {
+          this.favoriteCategories.push(categoryTemp[i]);
+        }
+        categoryTemp = null;
+      },
+      showPage: function (num) {
+        this.currentFavorites = this.pager.page(num);
+      },
+      nextPage: function () {
+        if (!this.pager.hasNext()) {
+          this.currentFavorites = this.pager.page(0);
+        } else {
+          this.currentFavorites = this.pager.next();
+        }
+        this.currentPage = this.pager.currentPage;
+      },
+      prevPage: function () {
+        if (this.pager.currentPage === 1) {
+          this.currentFavorites = this.pager.page(this.pager.totalPages);
+        } else {
+          this.currentFavorites = this.pager.prev();
+        }
+        this.currentPage = this.pager.currentPage;
+      },
+      edit: function (name) {
+        let index = nameExists(name, store.favorites);
+        this.indexToEdit = index;
+        this.editNoteCache = store.favorites[this.indexToEdit].notes;
+        store.favorites[this.indexToEdit].edit = true;
+      },
+      cancelEdit: function (name) {
+        let index = nameExists(name, store.favorites);
+        store.favorites[index].edit = false;
+        store.favorites[index].notes = this.editNoteCache;
+        this.indexToEdit = null;
+      },
+      update: function (newNote, name) {
+        console.log("update");
+        let itemIndex = nameExists(name, store.favorites);
+        store.favorites[itemIndex].notes = newNote;
+        store.favorites[itemIndex].edit = false;
+      },
+      removeItem: function (name) {
+        let itemIndex = nameExists(name, store.favorites);
+        store.favorites.splice(itemIndex, 1);
+        this.filteredFavorites = store.favorites;
+
+        // stay on filtered view
+        if (this.filterItem) {
+          this.filter(this.filterItem);
+        } else {
+          this.showAll();
+        }
+
+        // update dropdown
+        this.setFavoriteCategories(store.favorites);
+      },
+      filter: function (breed) {
+        this.filterItem = breed;
+        let filteredBreed = arr_filter(store.favorites, "breed", breed);
+
+        this.filteredFavorites = filteredBreed;
+        this.activatePager();
+      },
+      showAll: function () {
+        this.filterItem = null;
+        this.filteredFavorites = store.favorites;
+        this.activatePager();
+      },
+      showRandomDogImage: function () {
+        this.randomDogBreed = shuffle(allbreeds);
+        // Returns an array of all the images from the breed
+        let url = "https://dog.ceo/api/breed/" + this.randomDogBreed[0] + "/images";
+        let self = this;
+        axios_get(url)
+          .then(function (response) {
+            let arr = Object.values(response);
+            self.status.randomDog = "";
+            self.randomDogImage = shuffle(arr[0].message);
+          })
+          .then(function () {
+
+          });
+      },
+      switchBreed: function (breed) {
+        router.push({
+          path: "/gallery/" + breed
+        });
+      }
+    }
+};
 </script>
